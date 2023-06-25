86230655b215010b020cf8bbb9938edde1c73b7c
Fajar Indarto <101983078+fajarindarto@users.noreply.github.com>
2023-06-25 09:36:48 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Fajar Indarto" // TODO: replace this
+const studentId = "FE5044267" // TODO: replace this
+
+export { url, studentName, studentId }
